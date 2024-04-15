55eb469340f053bb51119dd7f169575c4048dc1d
dnthmn <domes-adopter.0b@icloud.com>
2024-04-16 00:35:42 +0200

    add example for module 1 - basic coding

--- main.js
+++ main.js
@@ -37,22 +37,21 @@
 
 // Return an array, where the ith element equals 1 if the triple (numbers[i], numbers[i + 1], numbers[i + 2]) is a zigzag, and 0 otherwise.
 
+const solution = (numbers) => {
+  const result = [];
 
-function solution(numbers) {
-    const result = [];
-    
-    for (let i = 0; i < numbers.length - 2; i++) {
-        const [a, b, c] = [numbers[i], numbers[i + 1], numbers[i + 2]];
-        
-        if ((a < b && b > c) || (a > b && b < c))
-            result.push(1);
-        else
-            result.push(0);
-    }
-    
-    return result;
-}
+  for (let i = 0; i < numbers.length - 2; i++) {
+    const [a, b, c] = [numbers[i], numbers[i + 1], numbers[i + 2]];
 
+    if ((a < b && b > c) || (a > b && b < c)) result.push(1);
+    else result.push(0);
+  }
+
+  return result;
+};
+
+console.log(solution([1, 2, 1, 3, 4]));
+console.log(solution([1, 2, 3, 4]));
 
 // Tests passed: 16/16.
 // Sample tests:
@@ -61,3 +60,38 @@ function solution(numbers) {
 // 8/8
 // Score:
 // 100/100
+
+// General Coding Skills Evaluation Framework
+// Framework Example Content
+// Module 1 – Basic Coding
+
+// Given an array a, your task is to output an array b of the same length by applying the following transformation:
+// • For each i from 0 to a.length - 1 inclusive, b[i] = a[i - 1] + a[i] + a[i + 1]
+// • If an element in the sum a[i - 1] + a[i] + a[i + 1] does not exist, use 0 in its place
+// • For instance, b[0] = 0 + a[0] + a[1]
+
+// Example
+// For a = [4, 0, 1, -2, 3]:
+// • b[0] = 0 + a[0] + a[1] = 0 + 4 + 0 = 4
+// • b[1] = a[0] + a[1] + a[2] = 4 + 0 + 1 = 5
+// • b[2] = a[1] + a[2] + a[3] = 0 + 1 + (-2) = -1
+// • b[3] = a[2] + a[3] + a[4] = 1 + (-2) + 3 = 2
+// • b[4] = a[3] + a[4] + 0 = (-2) + 3 + 0 = 1
+// So, the output should be solution(a) = [4, 5, -1, 2, 1]
+
+const solution1 = (a) => {
+  const result = [];
+
+  for (let i = 0; i <= a.length - 1; i++) {
+    const b =
+      (isNaN(a[i - 1]) ? 0 : a[i - 1]) +
+      (isNaN(a[i]) ? 0 : a[i]) +
+      (isNaN(a[i + 1]) ? 0 : a[i + 1]);
+
+    result.push(b);
+  }
+
+  return result;
+};
+
+console.log(solution1([4, 0, 1, -2, 3]));
