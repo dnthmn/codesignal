b18960b28bedcc60847c99d6257ccba6cd014dcf
dnthmn <domes-adopter.0b@icloud.com>
2024-04-16 16:28:40 +0200

    add example for module 2 - data manipulation

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ const solution = (numbers) => {
 // console.log(`Execution time: ${endPerformance0 - startPerformance0} ms`);
 
 console.log(solution([1, 2, 1, 3, 4]));
-console.log(solution([1, 2, 3, 4]));
+// console.log(solution([1, 2, 3, 4]));
 
 // Tests passed: 16/16.
 // Sample tests:
@@ -125,3 +125,74 @@ const solution1 = (a) => {
 // console.log(`Execution time: ${endPerformance1 - startPerformance1} ms`);
 
 console.log(solution1([4, 0, 1, -2, 3]));
+
+// Module 2 – Data Manipulation
+
+// You are given two strings: pattern and source. The first string pattern contains only the symbols 0 and 1, and
+// the second string source contains only lowercase English letters.
+
+// Your task is to calculate the number of substrings of source that match pattern.
+
+// We’ll say that a substring source[l..r] matches pattern if the following three conditions are met:
+// • The pattern and substring are equal in length.
+// • Where there is a 0 in the pattern, there is a vowel in the substring.
+// • Where there is a 1 in the pattern, there is a consonant in the substring.
+
+// Vowels are ["a", "e", "i", "o", "u", "y"]. All other letters are consonants.
+
+// Example
+// For pattern = "010" and source = "amazing", the output should be solution(pattern, source) = 2.
+// • "010" matches source[0..2] = "ama". The pattern specifies "vowel, consonant, vowel". "ama"
+// matches this pattern: 0 matches a, 1 matches m, and 0 matches a.
+// • "010" doesn’t match source[1..3] = "maz"
+// • "010" matches source[2..4] = "azi"
+// • "010" doesn’t match source[3..5] = "zin"
+// • "010" doesn’t match source[4..6] = "ing"
+// So, there are 2 matches.
+
+// For pattern = "100" and source = "codesignal", the output should be solution(pattern, source) = 0.
+// • There are no double vowels in the string "codesignal", so it’s not possible for any of its substrings to
+// match this pattern.
+
+// Guaranteed constraints:
+// • 1 ≤ source.length ≤ 10^3
+// • 1 ≤ pattern.length ≤ 10^3
+
+const solution2 = (pattern, source) => {
+  let result = 0;
+  const vowels = ["a", "e", "i", "o", "u", "y"];
+
+  for (let i = 0; i <= source.length - pattern.length; i++) {
+    let match = true;
+
+    for (let j = 0; j < pattern.length; j++) {
+      if (
+        (!vowels.includes(source[i + j]) && pattern[j] === "0") ||
+        (vowels.includes(source[i + j]) && pattern[j] === "1")
+      )
+        match = false;
+    }
+
+    if (match) result += 1;
+  }
+
+  return result;
+};
+
+// Measure performance
+// const startDate2 = Date.now();
+// solution2("010", "amazing");
+// const endDate2 = Date.now();
+// console.log(`Execution time: ${endDate2 - startDate2} ms`);
+
+// console.time("Execution Time");
+// solution2("010", "amazing");
+// console.timeEnd("Execution Time");
+
+// const startPerformance2 = performance.now();
+// solution2("010", "amazing");
+// const endPerformance2 = performance.now();
+// console.log(`Execution time: ${endPerformance2 - startPerformance2} ms`);
+
+console.log(solution2("010", "amazing"));
+// console.log(solution2("100", "codesignal"));
